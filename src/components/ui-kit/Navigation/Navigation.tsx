02cd36e9d00afa393443b31010c52397ae265d91
Aleksandr2905 <natalukha@ukr.net>
2024-07-23 17:37:48 +0300

    Update Navigation.tsx

--- src/components/ui-kit/Navigation/Navigation.tsx
+++ src/components/ui-kit/Navigation/Navigation.tsx
@@ -5,30 +5,33 @@ interface INavigationProps {
   onClick?: () => void;
 }
 
-function Navigation({ onClick }: INavigationProps) {
+const Navigation = ({ onClick }: INavigationProps) => {
   const { navigation } = data;
 
   return (
     <nav className="flex flex-col gap-12 my-auto items-center md:flex-row md:gap-14">
-      {navigation.map(({ to, title }) => (
-        <Link
-          key={to}
-          activeClass="active"
-          to={to}
-          href="#"
-          spy={true}
-          smooth={true}
-          offset={0}
-          duration={1000}
-          ignoreCancelEvents={true}
-          onClick={onClick}
-          className="flex text-lg tracking-[1.8px] font-normal md:text-sm md:tracking-[1.4px] hover:underline focus:underline focus:outline-white"
-        >
-          {title}
-        </Link>
-      ))}
+      <ul>
+        {navigation.map(({ to, title }) => (
+          <li key={to}>
+            <Link
+              activeClass="active"
+              to={to}
+              href="#"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={1000}
+              ignoreCancelEvents={true}
+              onClick={onClick}
+              className="flex text-lg tracking-[1.8px] font-normal md:text-sm md:tracking-[1.4px] hover:underline focus:underline focus:outline-white"
+            >
+              {title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
-}
+};
 
 export default Navigation;
