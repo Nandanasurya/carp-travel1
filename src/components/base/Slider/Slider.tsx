9896cae07390abdefb96de41d4619255f9d2ebdc
Aleksandr2905 <natalukha@ukr.net>
2024-07-25 22:46:27 +0300

    added Gallety

---
+++ src/components/base/Slider/Slider.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { useRef } from 'react';
+import Image from 'next/image';
+import data from '@/data/gallery.json';
+
+interface ISliderProps {
+  //   children: React.ReactNode;
+}
+
+const Slider = () => {
+  const refSlider = useRef(null);
+  const { slides } = data;
+
+  //   const handlePrev = () => {
+  //     refSlider.current.swiper.slidePrev(300);
+  //   };
+  //   const handleNext = () => {
+  //     refSlider.current.swiper.slideNext(300);
+  //   };
+
+  return (
+    <Swiper
+      direction="vertical"
+      slidesPerView={3}
+      spaceBetween={24}
+      modules={[Navigation]}
+      mousewheel={true}
+      pagination={{
+        clickable: true,
+      }}
+      loop={true}
+      style={{ height: '100vh' }}
+    >
+      <ul>
+        {slides.map((item, index) => (
+          <>
+            <SwiperSlide key={index}>
+              <Image
+                src={`/images/slides/gallery/gallery-0${item.path}-mobile@1x.webp`}
+                alt={item.alt}
+                width={606}
+                height={429}
+                priority
+                className="w-full h-full"
+              />
+            </SwiperSlide>
+          </>
+        ))}
+      </ul>
+    </Swiper>
+  );
+};
+
+export default Slider;
--- src/sections/Gallery.tsx
+++ src/sections/Gallery.tsx
@@ -1,9 +1,18 @@
+// 'use client';
+
 import Section from '@/components/base/Section/Section';
+import Slider from '@/components/base/Slider/Slider';
+import Title from '@/components/base/Title/Title';
+import GalleryCard from '@/components/ui-kit/GalleryCard/GalleryCard';
+import data from '@/data/gallery.json';
 
 const Gallery = () => {
+  const { title1, title2 } = data;
+
   return (
     <Section id="gallery">
-      <div>Gallery</div>
+      <Title title1={title1} title2={title2} />
+      <Slider />
     </Section>
   );
 };
