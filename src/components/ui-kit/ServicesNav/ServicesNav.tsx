8f9ca2d5d4ba972b62ae09cc5fc312ab3916aeb2
Aleksandr2905 <natalukha@ukr.net>
2024-07-29 17:14:40 +0300

    Update ServicesNav.tsx

--- src/components/ui-kit/ServicesNav/ServicesNav.tsx
+++ src/components/ui-kit/ServicesNav/ServicesNav.tsx
@@ -10,25 +10,27 @@ const ServicesNav = ({
   activeIndex,
 }: IServicesNavProps) => {
   return (
-    <ul className="flex flex-col gap-4 mt-6 mb-6 md:mt-0 xl:gap-6">
+    <div className="flex flex-col gap-4 mt-6 mb-6 md:mt-0 xl:gap-6">
       {slides.map(({ title }, index) => (
-        <li
+        // <li
+        //   key={index}
+        //   className={`flex items-center
+        //   ${activeIndex === index ? 'services-active' : ''}
+        // `}
+        // >
+        <button
           key={index}
+          type="button"
+          onClick={() => handleNavigationClick(index)}
+          aria-label={title}
           className={`flex items-center 
-          ${activeIndex === index ? 'services-active' : ''}
-        `}
+          ${activeIndex === index ? 'services-active' : ''} text-xl text-left font-extralight leading-[17px] uppercase  w-[186px] md:text-[22px] xl:text-[28px] xl:w-[236px] xl:leading-6 hover:opacity-100 hover:ml-2 focus:opacity-100  focus:outline-white ${activeIndex === index ? 'flex font-medium opacity-100' : 'opacity-50'}`}
         >
-          <button
-            type="button"
-            onClick={() => handleNavigationClick(index)}
-            aria-label={title}
-            className={`flex text-xl text-left font-extralight leading-[17px] uppercase  w-[186px] md:text-[22px] xl:text-[28px] xl:w-[236px] xl:leading-6 hover:opacity-100 hover:ml-2 focus:opacity-100  focus:outline-white ${activeIndex === index ? 'flex font-medium opacity-100' : 'opacity-50'}`}
-          >
-            {title}
-          </button>
-        </li>
+          {title}
+        </button>
+        // </li>
       ))}
-    </ul>
+    </div>
   );
 };
 
